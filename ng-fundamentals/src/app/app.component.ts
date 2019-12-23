edeee3d1edce872614bbb8db43d4ad68f1893221
Bryant Stewart Baldallaque <bbaldallaque@mardom.com>
2019-12-23 16:08:35 -0400

    Getting Started with Angular

---
+++ ng-fundamentals/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <img src="/assets/images/basic-shield.png"/>
+  `,
+  //templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ng-fundamentals';
+}
